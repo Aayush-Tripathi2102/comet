e96e0567fa5e40f527af43f67fc3a62aec667918
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2025-12-11 13:37:33 +0530

    fixed desktop mode

--- components/Appriciate.tsx
+++ components/Appriciate.tsx
@@ -10,20 +10,20 @@ const Appriciate = () => {
       <div className="flex w-full justify-center items-center py-10 bg-[#f5f5f5]">
         {/* Show all images on small screens, only first 3 on md+ */}
         <div className="flex w-full justify-center items-center">
-          <div className="flex">
+          <div className="flex items-center justify-between w-full">
             <Image
               src={"/Vogue.webp"}
               width={200}
               height={60}
               alt="Vogue"
-              className="w-1/3 sm:w-1/5 h-auto"
+              className="w-1/3 sm:w-1/6 h-auto"
             />
             <Image
               src={"/gq.avif"}
               width={200}
               height={60}
               alt="GQ"
-              className="w-1/3 sm:w-1/5 h-auto"
+              className="w-1/3 sm:w-1/6 h-auto"
             />
 
             <Image
@@ -31,22 +31,34 @@ const Appriciate = () => {
               width={200}
               height={60}
               alt="cosmopolitan"
-              className="w-1/3 sm:w-1/5 h-auto"
+              className="w-1/3 sm:w-1/6 h-auto"
             />
 
             <div className="hidden lg:flex">
-              <Image src={"/elle.webp"} width={200} height={60} alt="elle" />
+              <Image
+                src={"/elle.webp"}
+                width={200}
+                height={60}
+                alt="elle"
+                className="w-full h-auto"
+              />
+            </div>
+            <div className="hidden lg:flex">
               <Image
                 src={"/RollingStone.webp"}
                 width={200}
                 height={60}
                 alt="rs"
+                className="w-full h-auto"
               />
+            </div>
+            <div className="hidden lg:flex">
               <Image
                 src={"/grazia.webp"}
                 width={200}
                 height={60}
                 alt="grazia"
+                className="w-full  h-auto"
               />
             </div>
           </div>
