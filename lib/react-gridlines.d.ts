a2fa073a600cd2b9596d67c180a4030b9f183da0
Aayush-Tripathi2102 <aayushtripathi2102@gmail.com>
2025-12-11 13:20:02 +0530

    fixed gridline issue

---
+++ lib/react-gridlines.d.ts
@@ -0,0 +1,11 @@
+declare module "react-gridlines" {
+  import * as React from "react";
+  interface GridLinesProps {
+    className?: string;
+    cellWidth?: number;
+    cellWidth2?: number;
+    strokeWidth?: number;
+    children?: React.ReactNode;
+  }
+  const GridLines: React.FC<GridLinesProps>;
+  export default GridLines;}
