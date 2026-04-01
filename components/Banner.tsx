4797a4cdca198ad96b4a965989d882d169442544
Aayush Tripathi <138278278+Aayush-Tripathi2102@users.noreply.github.com>
2026-04-02 04:43:22 +0530

    chore: inject data-uid into components/Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,43 +1,21 @@
 import Image from "next/image";
 import React from "react";
-
 const Banner = () => {
-  return (
-    <div className="w-full relative text-5xl font-condensed font-bold bg-[#ECEB0B] text-[#1F1F1F] uppercase  py-12 border-b-2 border-black">
-      <div className="flex gap-8  items-center pb-8">
-        <Image
-          src="/cross.webp"
-          width={40}
-          height={40}
-          alt="cross"
-          className="hidden md:inline"
-        />
-        <Image
-          src="/star.webp"
-          width={40}
-          height={40}
-          alt="cross"
-          className="hidden md:inline"
-        />
-        <Image
-          src="/headphone.webp"
-          width={40}
-          height={40}
-          alt="cross"
-          className="hidden md:inline"
-        />
+  return <div className="w-full relative text-5xl font-condensed font-bold bg-[#ECEB0B] text-[#1F1F1F] uppercase  py-12 border-b-2 border-black" data-uid="element-1775085187957-122">
+      <div className="flex gap-8  items-center pb-8" data-uid="element-1775085187957-123">
+        <Image src="/cross.webp" width={40} height={40} alt="cross" className="hidden md:inline" data-uid="element-1775085187957-124" />
+        <Image src="/star.webp" width={40} height={40} alt="cross" className="hidden md:inline" data-uid="element-1775085187958-125" />
+        <Image src="/headphone.webp" width={40} height={40} alt="cross" className="hidden md:inline" data-uid="element-1775085187958-126" />
       </div>
-      <p className="bg-[#FF5271] px-6 py-4  ml-10 hidden md:inline-block rot-10 mr-2">
+      <p className="bg-[#FF5271] px-6 py-4  ml-10 hidden md:inline-block rot-10 mr-2" data-uid="element-1775085187958-127">
         good shoes
       </p>
-      <div className="md:hidden inline-block w-full py-4 bg-[#FF5271] rot-10 text-center text-4xl font-bold">
+      <div className="md:hidden inline-block w-full py-4 bg-[#FF5271] rot-10 text-center text-4xl font-bold" data-uid="element-1775085187958-128">
         good shoes
       </div>
-      <p className="mt-4 md:mt-0 ml-10 md:ml-0 inline-block">
+      <p className="mt-4 md:mt-0 ml-10 md:ml-0 inline-block" data-uid="element-1775085187958-129">
         Will take you to good places
       </p>
-    </div>
-  );
+    </div>;
 };
-
 export default Banner;
