d45b7aee5be69f3ca9e663395cf2250e6d275c98
Aayush Tripathi <138278278+Aayush-Tripathi2102@users.noreply.github.com>
2026-04-02 04:43:27 +0530

    chore: inject data-uid into components/GiftCard.tsx

--- components/GiftCard.tsx
+++ components/GiftCard.tsx
@@ -1,12 +1,8 @@
-import Image from 'next/image'
-import React from 'react'
-
+import Image from 'next/image';
+import React from 'react';
 const GiftCard = () => {
-  return (
-    <div className="cursor-pointer">
-        <Image src={"/giftCard.webp"} width={2000} height={1000} alt="Comet Banner" />
-    </div>
-  )
-}
-
-export default GiftCard
+  return <div className="cursor-pointer" data-uid="element-1775085187972-240">
+        <Image src={"/giftCard.webp"} width={2000} height={1000} alt="Comet Banner" data-uid="element-1775085187972-241" />
+    </div>;
+};
+export default GiftCard;
