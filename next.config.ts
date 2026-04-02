d9dad4b7ca0b0308f102016d173952e92339c3b7
Aayush Tripathi <138278278+Aayush-Tripathi2102@users.noreply.github.com>
2026-04-02 12:58:29 +0530

    chore: inject data-uid into next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,12 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.wearcomet.com',
-        pathname: '/cdn/shop/files/**',
-      },
-    ],
-  },
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'www.wearcomet.com',
+      pathname: '/cdn/shop/files/**'
+    }]
+  }
   /* config options here */
 };
-
 export default nextConfig;
