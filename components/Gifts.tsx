0304b8b045a0580f2b69939f4258170c80a7ff3f
Aayush Tripathi <138278278+Aayush-Tripathi2102@users.noreply.github.com>
2026-04-02 12:58:11 +0530

    chore: inject data-uid into components/Gifts.tsx

--- components/Gifts.tsx
+++ components/Gifts.tsx
@@ -1,39 +1,11 @@
 import Image from "next/image";
 import React from "react";
-
 const Gifts = () => {
-  return (
-    <div className="w-full flex">
-      <Image
-        src="/wrapped.webp"
-        width={1920}
-        height={1080}
-        alt="gifts"
-        className="w-1/2 h-auto hidden md:block"
-      />
-      <Image
-        src="/coffee.webp"
-        width={1920}
-        height={1080}
-        alt="gifts"
-        className="w-1/2 h-auto hidden md:block"
-      />
-      <Image
-        src="/wrapped-mobile.jpg"
-        width={1920}
-        height={1080}
-        alt="gifts"
-        className="w-1/2 h-auto block md:hidden"
-      />
-      <Image
-        src="/coffee.jpg"
-        width={1920}
-        height={1080}
-        alt="gifts"
-        className="w-1/2 h-auto block md:hidden"
-      />
-    </div>
-  );
+  return <div className="w-full flex" data-uid="element-1775114870161-242">
+      <Image src="/wrapped.webp" width={1920} height={1080} alt="gifts" className="w-1/2 h-auto hidden md:block" data-uid="component-1775114870161-243" />
+      <Image src="/coffee.webp" width={1920} height={1080} alt="gifts" className="w-1/2 h-auto hidden md:block" data-uid="component-1775114870161-244" />
+      <Image src="/wrapped-mobile.jpg" width={1920} height={1080} alt="gifts" className="w-1/2 h-auto block md:hidden" data-uid="component-1775114870161-245" />
+      <Image src="/coffee.jpg" width={1920} height={1080} alt="gifts" className="w-1/2 h-auto block md:hidden" data-uid="component-1775114870161-246" />
+    </div>;
 };
-
 export default Gifts;
